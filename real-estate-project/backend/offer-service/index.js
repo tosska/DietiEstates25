6e94cfef2f14026bd63c9ebf21b900ea7854ebab
tosska <151936621+tosska@users.noreply.github.com>
2025-05-05 18:16:39 +0200

    Inizializzazione progetto: struttura base e configurazione back-end

---
+++ real-estate-project/backend/offer-service/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express(); // creates an express application
+const PORT = 3004;
+
+// Register the morgan logging middleware, use the 'dev' format
+app.use(morgan('dev'));
+
+app.use(cors()); 
+
+// Parse incoming requests with a JSON payload
+app.use(express.json());
+
+//error handler
+app.use( (err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500).json({
+        code: err.status || 500,
+        description: err.message || "An error occurred"
+    });
+});
+
+
+app.listen(PORT);
